aa44fc05211e7ac420c136cfb501a04be68d7dea
Ana Katariny de Souza Cacheta <anakatariny@gmail.com>
2018-06-17 14:01:41 -0300

    Primeira versão do model

--- shortest_route_app/models.py
+++ shortest_route_app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Map(models.Model):
+    name = models.CharField(max_length=300)
+    path = models.CharField(max_length=300)
+    created_date = models.DateTimeField(
+            default=timezone.now)
+
+    def uploadMap(self):
+        #TODO implement this method
+
+    def __str__(self):
+        return self.name
+
+
+class Route(models.Model):
+    map_id = models.ForeignKey('models.Map', on_delete=models.CASCADE)
+    origin = models.CharField(max_length=200)
+    destiny = models.CharField(max_length=200)
+    distance = models.FloatField()
+    created_date = models.DateTimeField(
+            default=timezone.now)
+
+    def getRoute(self):
+        #TODO implement this method
+
+    def __str__(self):
+        return self.origin, self.destiny
