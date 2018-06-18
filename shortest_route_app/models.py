6b36f6858ff488e6e6f5f004d28f9438f22f74c2
Ana Katariny de Souza Cacheta <anakatariny@gmail.com>
2018-06-17 21:11:28 -0300

    criação do modelo de banco de dados (correções e migração)

--- shortest_route_app/models.py
+++ shortest_route_app/models.py
@@ -1,29 +1,28 @@
 from django.db import models
 from django.utils import timezone
 
+#this class take care of the map information through the file
 class Map(models.Model):
-    name = models.CharField(max_length=300)
+    name = models.CharField(max_length=300, unique=True)
     path = models.CharField(max_length=300)
-    created_date = models.DateTimeField(
-            default=timezone.now)
+    created_date = models.DateTimeField(default=timezone.now)
 
-    def uploadMap(self):
+    #def uploadMap(self):
         #TODO implement this method
 
     def __str__(self):
         return self.name
 
-
+#this class take care of all the routes informations in each map
 class Route(models.Model):
-    map_id = models.ForeignKey('models.Map', on_delete=models.CASCADE)
+    map_id = models.ForeignKey('shortest_route_app.Map', on_delete=models.CASCADE)
     origin = models.CharField(max_length=200)
     destiny = models.CharField(max_length=200)
     distance = models.FloatField()
-    created_date = models.DateTimeField(
-            default=timezone.now)
+    created_date = models.DateTimeField(default=timezone.now)
 
-    def getRoute(self):
+    #def getRoute(self):
         #TODO implement this method
 
     def __str__(self):
-        return self.origin, self.destiny
+        return [self.origin, self.destiny, self.distance]
