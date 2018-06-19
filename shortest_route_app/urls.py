66a33011b80647955a95d33d1d167f564cead6dd
Ana Katariny de Souza Cacheta <anakatariny@gmail.com>
2018-06-19 20:25:57 -0300

    Correção da API para o funcionamento de GET, POST e DELETE

--- shortest_route_app/urls.py
+++ shortest_route_app/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
-from .views import FileMapView
+from .views import FileMapView, FileMapDetail
+
 urlpatterns = [
-  url(r'^map/$', FileMapView.as_view(), name='file-upload'),
+    url(r'^map/$', FileMapView.as_view(), name='file-upload'),
+    url(r'^map/(?P<pk>[0-9]+)/$', FileMapDetail.as_view()),
 ]
--- shortest_route_app/views.py
+++ shortest_route_app/views.py
@@ -1,12 +1,16 @@
 from rest_framework.views import APIView
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, serializers
+from django.http import Http404
 from .serializers import FileMapSerializer
 from .models import FileMap
 
-#This class contain API handlers to manipulate the file map with methods GET, POST and DELETE
+
 class FileMapView(APIView):
+    """
+    This class contain API handlers to manipulate the file map with methods GET, POST and DELETE
+    """
     parser_classes = (MultiPartParser, FormParser)
     serializer_class = FileMapSerializer
     queryset = FileMap.objects.all()
@@ -15,7 +19,7 @@ class FileMapView(APIView):
         mapsList = FileMap.objects.all()
         result = []
         for map in mapsList:
-            result.append(map.name)
+            result.append("id:"+str(map.id)+"; nome:"+map.name)
 
         return Response({'maps':result})
 
@@ -23,11 +27,40 @@ class FileMapView(APIView):
         file_serializer = FileMapSerializer(data=request.data)
         if file_serializer.is_valid():
             file_serializer.save()
+
             return Response(file_serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(file_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    def perform_create(self, serializer):
+        serializer.ValidationError("Can't open file.")
+
+
+class FileMapDetail(APIView):
+    """
+    Retrieve, update or delete a fileMap instance.
+    """
+
+    def get_object(self, pk):
+        try:
+            return FileMap.objects.get(pk=pk)
+        except FileMap.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        fileMap = self.get_object(pk)
+        serializer = FileMapSerializer(fileMap)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        fileMap = self.get_object(pk)
+        serializer = FileMapSerializer(fileMap, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
     def delete(self, request, pk, format=None):
-        object = self.get_object(pk)
-        object.delete()
+        fileMap = self.get_object(pk)
+        fileMap.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
